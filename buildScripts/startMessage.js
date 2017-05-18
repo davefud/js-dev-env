c933eaa3a63f82a5a08a7509d6c65c17bd63e7f2
David Glass <davefud@gmail.com>
2017-05-18 13:37:54 -0400

    Added lint and lint watch to build process.

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
-var chalk = require('chalk');
+//var chalk = require('chalk'); // CommonJS
+import chalk from 'chalk'; // ES6 Modules
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
